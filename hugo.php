d25f66bf2140c00d11555988f8a2425ed6fc4b4e
Hugo <Hugo@08formadormac.local>
2015-07-14 09:35:23 +0200

    Commit 1 Hugo

---
+++ hugo.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * PROYECTO DE PRUEBA PARA 'GitHub'
+ * https://github.com/enblabla/ioskike.git
+ */
+
+/**
+ * @author Hugo
+ * @version 0.01
+ */
+
+function saludar(){
+    print 'hola soy Hugo';
+}
