897a538a69be8e3701daec0f0a5cf9933feebef1
ciceroot <ciceroot@gmail.com>
2015-07-14 09:44:36 +0200

    cambios al fichero prueba2

--- prueba.php
+++ prueba.php
@@ -4,6 +4,12 @@ function saludar(){
     print 'hola mundo soy kike';
 }
 
+
+function saludar2(){
+    print 'hola mundo soy kike';
+}
+
+
 //Funcion de prueba 2
 function hola(){
     echo 'Hola soy Javier';
